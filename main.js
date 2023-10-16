672094548c2c387204eb821e107a4479dac2d8d2
dmitri-coder <zverkov.dmitri@gmail.com>
2023-10-16 10:35:42 +0300

    add inquirer examples

--- main.js
+++ main.js
@@ -1,45 +1,30 @@
-import chalk from 'chalk';
-import boxen from 'boxen';
-import readline from 'readline';
+import inquirer from 'inquirer';
 
 function write(text) {
     process.stdout.write(text);
 }
-console.clear();
-readline.emitKeypressEvents(process.stdin);
-
-if(process.stdin.isTTY){
-    process.stdin.setRawMode(true);
-}
 
 
-write('@');
-
-process.stdin.on('keypress', (chunk, key) => {
-    if(key && key.name === 'c' && key.ctrl === true){
-        process.exit();
+inquirer
+  .prompt([
+    {type:'input', name:'name', message: 'What is your name?'},
+    {type:'list', name:'gender', message: 'What is your gender?', choices: ['male', 'female', 'bigender', 'apache helicopetr', 'other']},
+    {type:'number', name:'number', message: 'What is your number?'},
+    {type:'confirm', name:'Prayed', message: 'Did you pray today?'},
+    {type:'rawlist', name:'sus', message: 'How sus are you?', choices: ['very','impostor type','a lil','not sus']},
+    {type:'password', name:'Password', message: 'Type your password'},
+    {type:'checkbox', name:'prayed', message: 'Did you pray?', choices: ['Yes, i did','No, I didnt','fuck you']},
+    {type:'editor', name:'editor', message: 'Editor'},
+    {type:'expand', name:'expand', message: 'Conflict on file amongus', choices: ['fuck it','delete it','ignore']},
+])
+  .then((answers) => {
+    console.log(answers)
+    {checked: true}
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
     }
-    if(key && key.name === 'd'){
-       write('\x1B[1D');
-       write(' @'); 
-    }
-    if(key && key.name === 's'){
-        write('\x1B[1D');
-        write(' ');
-        write('\x1B[1D');
-        write('\x1B[1B');
-        write('@');
-    }
-    if(key && key.name === 'a'){
-        write('\x1B[2D');
-        write('@ '); 
-        write('\x1B[1D');
-    }
-    if(key && key.name === 'w'){
-        write('\x1B[1D');
-        write(' ');
-        write('\x1B[1D');
-        write('\x1B[1A');
-        write('@');
-    }
-});
+  });
