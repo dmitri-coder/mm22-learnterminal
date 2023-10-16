6931dd7be6cb4567a2d08cc66758e700ab04f23c
dmitri-coder <zverkov.dmitri@gmail.com>
2023-10-16 08:56:57 +0300

    chalk

---
+++ main.js
@@ -0,0 +1,14 @@
+import chalk from 'chalk';
+import boxen from 'boxen';
+
+function write(text) {
+    process.stdout.write(text);
+}
+
+console.log(chalk.bgGreen.magenta('Hello world'));
+console.log(chalk.bgGreen.magenta.underline('Hello world'));
+console.log(chalk.italic('Hello world'));
+console.log(chalk.strikethrough('Hello world'));
+console.log(chalk.inverse('Hello world')); 
+
+console.log(boxen(chalk.bgGreen.magenta('Hello world'), {borderStyle:'round'}));
